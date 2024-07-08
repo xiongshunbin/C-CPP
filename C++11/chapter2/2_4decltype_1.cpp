3a82269c9a847cf2468c4023cf4868df57745b81
mushan <xiongshunbin@outlook.com>
2024-07-08 23:28:32 +0800

    2024.7.8

--- C++11/chapter2/2_4decltype_1.cpp
+++ C++11/chapter2/2_4decltype_1.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+class Test
+{
+public:
+    int num = 9;
+    string text;
+    static const int value = 110;
+};
+
 int main()
 {
-    
+    int x = 99;
+    const int &y = x;
+    // a : int
+    decltype(x) a = x;
+    // y : const int&
+    // b : const int &
+    decltype(y) b = x;
+    // const int
+    decltype(Test::value) c = 0;
+    Test t;
+    // string
+    decltype(t.text) d = "hello, world";
     return 0;
 }
