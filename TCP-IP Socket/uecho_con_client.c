b7ee75d4b22f7a9a3e96806ae5deafac724166e9
mushan <xiongshunbin@outlook.com>
2024-07-20 14:55:20 +0800

    uecho_con_client

--- TCP-IP Socket/uecho_con_client.c
+++ TCP-IP Socket/uecho_con_client.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
     int sock;
     char message[BUF_SIZE];
     int str_len;
-    socklen_t adr_sz;
+    socklen_t adr_sz; // 多余变量！
 
-    struct sockaddr_in serv_adr, from_adr;
+    struct sockaddr_in serv_adr, from_adr; // 不再需要from_adr!
     if (argc != 3)
     {
         printf("Usage : %s <IP> <port>", argv[0]);
@@ -30,18 +30,26 @@ int main(int argc, char *argv[])
     serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_adr.sin_port = htons(atoi(argv[2]));
 
+    connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
+
     while (1)
     {
         fputs("Insert message(q to quit): ", stdout);
         fgets(message, sizeof(message), stdin);
         if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
             break;
-
+        /*
         sendto(sock, message, strlen(message), 0,               // 向服务器传输数据
                 (struct sockaddr *)&serv_adr, sizeof(serv_adr)); // 接收数据
+        */
+        write(sock, message, strlen(message));
+        /*
         adr_sz = sizeof(from_adr);
         str_len = recvfrom(sock, message, BUF_SIZE, 0,
                 (struct sockaddr *)&from_adr, &adr_sz);
+        */
+        str_len = read(sock, message, sizeof(message) - 1);
+        
         message[str_len] = 0;
         printf("Message from server: %s", message);
     }
