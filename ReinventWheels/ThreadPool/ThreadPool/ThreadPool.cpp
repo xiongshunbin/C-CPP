7cefc3433f5737bac3ebaa6530ccf13e6ce32dc5
mushan <xiongshunbin@outlook.com>
2025-02-26 23:15:19 +0800

    implement async threadpool with CPP11

--- ReinventWheels/ThreadPool/ThreadPool/ThreadPool.cpp
+++ ReinventWheels/ThreadPool/ThreadPool/ThreadPool.cpp
@@ -1,8 +1,152 @@
 #include "ThreadPool.h"
 
+#include <iostream>
+
 ThreadPool::ThreadPool(int min, int max) : m_minThread(min), m_maxThread(max),
 m_curThread(min), m_idleThread(min), m_stop(false)
 {
 	// 创建管理者线程
-	m_manager = new std::thread()
+	m_manager = new std::thread(&ThreadPool::manager, this);
+
+	// 创建工作者线程
+	for (int i = 0; i < min; i++)
+	{
+		std::thread t(&ThreadPool::worker, this);
+		m_workers.insert(std::make_pair(t.get_id(), std::move(t)));
+	}
+}
+
+ThreadPool::~ThreadPool()
+{
+	m_stop = true;
+	m_condition.notify_all();
+	for (auto& it : m_workers)
+	{
+		std::thread& t = it.second;
+		if (t.joinable())
+		{
+			std::cout << "********** Thread " << t.get_id() << " will exit......" << std::endl;
+			t.join();
+		}
+	}
+
+	if (m_manager->joinable())
+	{
+		m_manager->join();
+	}
+	delete m_manager;
+}
+
+void ThreadPool::addTask(std::function<void(void)> task)
+{
+	{
+		std::lock_guard<std::mutex> locker(m_mutexQueue);
+		m_taskQ.emplace(task);
+	}
+
+	m_condition.notify_one();
+}
+
+void ThreadPool::manager(void)
+{
+	while (!m_stop.load())
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		int idle = m_idleThread.load();
+		int cur = m_curThread.load();
+		if (idle > cur / 2 && cur > m_minThread)
+		{
+			// 每次销毁两个线程
+			m_exitThread.store(2);
+			m_condition.notify_all();
+			std::lock_guard<std::mutex> lck(m_mutexIds);
+			for (auto id : m_ids)
+			{
+				auto it = m_workers.find(id);
+				if (it != m_workers.end())
+				{
+					std::cout << "==========The thread " << (*it).first << " is destroyed." << std::endl;
+					(*it).second.join();
+					m_workers.erase(it);
+				}
+			}
+			m_ids.clear();
+		}
+		else if (idle == 0 && cur < m_maxThread)
+		{
+			std::thread t(&ThreadPool::worker, this);
+			m_workers.insert(std::make_pair(t.get_id(), std::move(t)));
+			std::cout << "The thread is created......" << std::endl;
+			m_curThread++;
+			m_idleThread++;
+		}
+	}
+}
+
+void ThreadPool::worker(void)
+{
+	while (!m_stop.load())
+	{
+		std::function<void(void)> task = nullptr;
+		{
+			std::unique_lock<std::mutex> locker(m_mutexQueue);
+			while (m_taskQ.empty() && !m_stop)
+			{
+				m_condition.wait(locker);
+				if (m_exitThread.load() > 0)
+				{
+					m_curThread--;
+					m_idleThread--;
+					m_exitThread--;
+					std::cout << "----------The thread exited, ID: " << std::this_thread::get_id() << std::endl;
+					std::lock_guard<std::mutex> lck(m_mutexIds);
+					m_ids.emplace_back(std::this_thread::get_id());
+					return;
+				}
+			}
+			if (!m_taskQ.empty())
+			{
+				std::cout << "Took out a task......" << std::endl;
+				task = std::move(m_taskQ.front());
+				m_taskQ.pop();
+			}
+		}
+
+		if (task)
+		{
+			m_idleThread--;
+			task();
+			m_idleThread++;
+		}
+	}
+}
+
+void calc(int x, int y)
+{
+	int z = x + y;
+	std::cout << "z = " << z << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+}
+
+int calc1(int x, int y)
+{
+	int z = x + y;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	return z;
+}
+
+int main()
+{
+	ThreadPool pool;
+	std::vector<std::future<int>> results;
+	for (int i = 0; i < 10; i++)
+	{
+		//auto obj = std::bind(calc, i, i * 2);
+		results.emplace_back(pool.addTask(calc1, i, i * 2));
+	}
+	for (auto& item : results)
+	{
+		std::cout << "The thread's results: " << item.get() << std::endl;
+	}
+	return 0;
 }
--- ReinventWheels/ThreadPool/ThreadPool/ThreadPool.h
+++ ReinventWheels/ThreadPool/ThreadPool/ThreadPool.h
@@ -2,11 +2,15 @@
 
 #include <thread>
 #include <vector>
+#include <map>
 #include <atomic>
 #include <queue>
 #include <functional>
 #include <mutex>
-#include <condition_variable>s
+#include <future>
+#include <memory>
+#include <condition_variable>
+
 
 /*
 * 构成：
@@ -14,7 +18,7 @@
 *		- 控制工作线程的数量：增加或减少
 *	2.工作者线程	->	子线程，n个
 *		- 从任务队列中取任务，并处理
-*		- 若任务队列为空，被阻塞（被条件变量阻塞）
+*		- 若任务队列为空，条件变量阻塞
 *		- 线程同步（互斥锁）
 *		- 最小、最大、当前、空闲线程数量
 *	3.任务队列	->	STL	-> queue
@@ -22,22 +26,54 @@
 *		- 条件变量
 *	4. 线程池开关	-> bool
 */
+
 class ThreadPool
 {
 public:
 	ThreadPool(int min = 2, int max = std::thread::hardware_concurrency());
 	~ThreadPool();
 
+	// 添加任务	->	任务队列
+	void addTask(std::function<void(void)> task);
+
+	template <typename F, typename... Args>
+	auto addTask(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>
+	{
+		// 1. 创建package_task实例
+		using returnType = typename std::result_of<F(Args...)>::type;
+		auto mytask = std::make_shared<std::packaged_task<returnType()>> (
+			std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+			);
+		// 2. 得到future
+		std::future<returnType> res = mytask->get_future();
+		// 3. 任务函数添加到任务队列
+		m_mutexQueue.lock();
+		m_taskQ.emplace([mytask]() {
+			(*mytask)();
+			});
+		m_mutexQueue.unlock();
+		m_condition.notify_one();
+
+		return res;
+	}
+
 private:
-	std::thread* m_manager;
-	std::vector<std::thread> m_workers;
-	std::atomic<int> m_minThread;
-	std::atomic<int> m_maxThread;
-	std::atomic<int> m_curThread;
-	std::atomic<int> m_idleThread;
-	std::atomic<bool> m_stop;
-	std::queue<std::function<void(void)>> m_task;
-	std::mutex m_mutexQueue;
-	std::condition_variable m_condition;
+	void manager(void);
+	void worker(void);
+
+private:
+	std::thread* m_manager;								// 管理者线程
+	std::map<std::thread::id, std::thread> m_workers;	// 工作者线程
+	std::vector<std::thread::id> m_ids;					// 退出线程函数的线程ID
+	std::atomic<int> m_minThread;						// 最小线程数
+	std::atomic<int> m_maxThread;						// 最大线程数
+	std::atomic<int> m_curThread;						// 当前线程数
+	std::atomic<int> m_idleThread;						// 空闲线程数
+	std::atomic<int> m_exitThread;						// 退出线程数
+	std::atomic<bool> m_stop;							// 线程池开关
+	std::queue<std::function<void(void)>> m_taskQ;		// 任务队列
+	std::mutex m_mutexQueue;							// 互斥锁，用于对任务队列的互斥访问
+	std::mutex m_mutexIds;								// 互斥锁，用于对，m_ids的互斥访问
+	std::condition_variable m_condition;				// 条件变量，当任务队列为空时，阻塞消费者进程
 };
 
