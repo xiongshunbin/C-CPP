29809056dc674edc5395313fa149dfcb6793c0d5
mushan <xiongshunbin@outlook.com>
2026-02-10 23:52:04 +0800

    2026.2.10

--- C++_Concurrency/C++_Concurrency_In_Action/p03.cpp
+++ C++_Concurrency/C++_Concurrency_In_Action/p03.cpp
@@ -150,6 +150,7 @@ public:
 		return res;
 	}
 
+	// 使用引用减少返回值的拷贝
 	void pop(T& value)
 	{
 		std::lock_guard<std::mutex> lock(m);
@@ -187,7 +188,7 @@ void test_threadsafe_stack()
 			}
 			catch (const empty_stack& e)
 			{
-				std::cout << "stack is empty!" << std::endl;
+				std::cout << e.what() << std::endl;
 			}
 		}
 	});
@@ -212,6 +213,127 @@ void test_threadsafe_stack()
 	t2.join();
 }
 
+std::mutex t_lock1;
+std::mutex t_lock2;
+int m_1 = 0;
+int m_2 = 1;
+
+void dead_lock1()
+{
+	while (true)
+	{
+		std::cout << "dead_lock1 begin..." << std::endl;
+		t_lock1.lock();
+		m_1 = 1024;
+		t_lock2.lock();
+		m_2 = 2048;
+		t_lock2.unlock();
+		t_lock1.unlock();
+		std::cout << "dead_lock1 end..." << std::endl;
+	}
+}
+
+void dead_lock2()
+{
+	while (true)
+	{
+		std::cout << "dead_lock2 begin..." << std::endl;
+		t_lock2.lock();
+		m_2 = 2048;
+		t_lock1.lock();
+		m_1 = 1024;
+		t_lock1.unlock();
+		t_lock2.unlock();
+		std::cout << "dead_lock2 end..." << std::endl;
+	}
+}
+
+void test_dead_lock()
+{
+	std::thread t1(dead_lock1);
+	std::thread t2(dead_lock2);
+	t1.join();
+	t2.join();
+}
+
+// 加锁和解锁作为原子操作解耦合, 各自只管理自己的功能
+void atomic_lock1()
+{
+	std::cout << "lock1 begin lock" << std::endl;
+	t_lock1.lock();
+	m_1 = 1024;
+	t_lock1.unlock();
+	std::cout << "lock1 end lock" << std::endl;
+}
+
+void atomic_lock2()
+{
+	std::cout << "lock2 begin lock" << std::endl;
+	t_lock2.lock();
+	m_2 = 2048;
+	t_lock2.unlock();
+	std::cout << "lock2 end lock" << std::endl;
+}
+
+void safe_lock1()
+{
+	while (true)
+	{
+		atomic_lock1();
+		atomic_lock2();
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+}
+
+void safe_lock2()
+{
+	while (true)
+	{
+		atomic_lock2();
+		atomic_lock1();
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+}
+
+void test_safe_lock()
+{
+	std::thread t1(safe_lock1);
+	std::thread t2(safe_lock2);
+	t1.join();
+	t2.join();
+}
+
+// 对于要使用两个互斥量, 可以同时加锁, 如不同时加锁, 可能会死锁
+class some_big_object
+{
+public:
+	some_big_object(int data) : _data(data) { }
+	// 拷贝构造
+	some_big_object(const some_big_object& b2) : _data(b2._data) { }
+	// 移动构造
+	some_big_object(some_big_object&& b2) noexcept : _data(std::move(b2._data)) { }
+	// 拷贝赋值运算符重载
+	some_big_object& operator=(const some_big_object& b2)
+	{
+		if (this != &b2)
+		{
+			_data = b2._data;
+		}
+		return *this;
+	}
+	// 输出运算符重载
+	friend std::ostream& operator<<(std::ostream& os, const some_big_object& big_obj)
+	{
+		os << big_obj._data;
+		return os;
+	}
+
+
+private:
+	int _data;
+};
+
+
 #if 1
 
 int main()
@@ -220,7 +342,16 @@ int main()
 
 	// test_threadsafe_stack1();
 
-	test_threadsafe_stack();
+	// test_threadsafe_stack();
+
+	// test_dead_lock();
+
+	// test_safe_lock();
+
+	some_big_object bigobj1(100);
+	some_big_object bigobj2(200);
+
+	bigobj2 = bigobj1;
 
 	return 0;
 }
