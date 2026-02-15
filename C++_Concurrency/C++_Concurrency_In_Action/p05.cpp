79169e38d63a9d82047e40071cb809b98b3ffef7
mushan <xiongshunbin@outlook.com>
2026-02-15 23:19:47 +0800

    2026.2.15

--- C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
+++ C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
@@ -2,11 +2,33 @@
 #include <thread>
 #include <mutex>
 
+class Single2
+{
+private:
+	Single2() { }
+	Single2(const Single2&) = delete;
+	Single2& operator=(const Single2&) = delete;
+
+public:
+	static Single2& getInstance()
+	{
+		static Single2 single;
+		return single;
+	}
+};
+
+void test_single2()
+{
+	std::cout << "single1 address: " << &Single2::getInstance() << std::endl;
+	std::cout << "single2 address: " << &Single2::getInstance() << std::endl;
+}
+
 #if 1
 
 int main()
 {
-
+	test_single2();
+	return 0;
 }
 
 #endif
