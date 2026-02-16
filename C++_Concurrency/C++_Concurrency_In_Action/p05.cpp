6fd595b53b8c2c637973a6696cbda439335f5653
mushan <xiongshunbin@outlook.com>
2026-02-16 23:02:48 +0800

    2026.2.16

--- C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
+++ C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
@@ -23,6 +23,47 @@ void test_single2()
 	std::cout << "single2 address: " << &Single2::getInstance() << std::endl;
 }
 
+// ¶öººÊ½
+class Single2Hungry
+{
+private:
+	Single2Hungry() { }
+	Single2Hungry(const Single2Hungry&) = delete;
+	Single2Hungry& operator=(const Single2Hungry&) = delete;
+
+public:
+	static Single2Hungry* getInstance()
+	{
+		if (single == nullptr)
+		{
+			single = new Single2Hungry;
+		}
+		return single;
+	}
+
+private:
+	static Single2Hungry* single;
+};
+
+Single2Hungry* Single2Hungry::single = Single2Hungry::getInstance();
+
+void thread_func_s2(int i)
+{
+	std::cout << "Thread[" << i << "]: " << Single2Hungry::getInstance() << std::endl;
+}
+
+void test_single2hungry()
+{
+	std::cout << "Single1 address: " << Single2Hungry::getInstance() << std::endl;
+	std::cout << "Single2 address: " << Single2Hungry::getInstance() << std::endl;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		std::thread tid(thread_func_s2, i);
+		tid.join();
+	}
+}
+
 #if 1
 
 int main()
