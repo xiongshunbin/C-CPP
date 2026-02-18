d8e9cb9d779f6e693961c4f7ab421809d8c50026
mushan <xiongshunbin@outlook.com>
2026-02-18 12:09:54 +0800

    2026.2.18

--- C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
+++ C++_Concurrency/C++_Concurrency_In_Action/p05.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <memory>
 #include <chrono>
+#include <functional>
 
 class Single2
 {
@@ -101,7 +102,7 @@ private:
 
 std::shared_ptr<SingletonOnce> SingletonOnce::_instance = nullptr;
 
-void TestSingle()
+void test_singletonOnce()
 {
 	std::thread t1([]() {
 		std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -118,9 +119,84 @@ void TestSingle()
 }
 
 template<typename T>
+class Singleton
+{
+protected:
+	Singleton() { }
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+public:
+	static std::shared_ptr<T> getInstance()
+	{
+		static std::once_flag flag;
+		std::call_once(flag, [&]() {
+			_instance = std::shared_ptr<T>(new T, T::_deletor);
+		});
+		return _instance;
+	}
+
+	void printAddress()
+	{
+		std::cout << _instance.get() << std::endl;
+	}
+
+	~Singleton()
+	{
+		std::cout << "Singleton destructed!" << std::endl;
+	}
+
+private:
+	static std::shared_ptr<T> _instance;
+};
+
+template<typename T>
+std::shared_ptr<T> Singleton<T>::_instance = nullptr;
+
+class LogicSystem : public Singleton<LogicSystem>
+{
+	friend class Singleton<LogicSystem>;
+	friend void SafeDeletor(LogicSystem* other);
+private:
+	LogicSystem()
+	{
+	}
+
+	~LogicSystem()
+	{
+		std::cout << "LogicSystem destructed!" << std::endl;
+	}
+
+private:
+	static std::function<void(LogicSystem*)> _deletor;
+};
+
+void SafeDeletor(LogicSystem* other)
+{
+	delete other;
+}
+
+std::function<void(LogicSystem*)> LogicSystem::_deletor = SafeDeletor;
 
 
-#if 1
+
+void test_singleton()
+{
+	std::thread t1([]() {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		LogicSystem::getInstance()->printAddress();
+	});
+
+	std::thread t2([]() {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		LogicSystem::getInstance()->printAddress();
+	});
+
+	t1.join();
+	t2.join();
+}
+
+#if 0
 
 int main()
 {
@@ -128,7 +204,9 @@ int main()
 
 	// test_single2hungry();
 
-	TestSingle();
+	// test_singletonOnce();
+
+	test_singleton();
 
 	return 0;
 }
